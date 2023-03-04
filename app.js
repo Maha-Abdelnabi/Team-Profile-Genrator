1573760d66dd43565105ed018c545b64ddc10f5f
Maha Abdelnabi <mahaomar@gmail.com>
2023-03-04 01:01:56 -0500

    create prompts for employees

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./lib/employee.js");
 const Maneger = require("./lib/maneger.js");
 const Engineer = require("./lib/engineer.js");
 const Intern = require("./lib/intern.js");
@@ -14,15 +13,15 @@ function chooseEmployee() {
         type: "list",
         name: "employee",
         message: "What is the employee's role?",
-        choices: ["Manager", "Engineer", "Intern"],
+        choices: ["Maneger", "Engineer", "Intern"],
       },
     ])
     .then((response) => {
       console.log(response);
-
+      //depending on the user response, call the function then stop
       switch (response.employee) {
-        case "Manager":
-          createManager();
+        case "Maneger":
+          createManeger();
           break;
         case "Engineer":
           createEngineer();
@@ -30,10 +29,123 @@ function chooseEmployee() {
         case "Intern":
           createIntern();
           break;
-        default:
-          buildTeam();
+       
       }
     });
 }
 
+
+//create maneger prompt
+const teamMembers = [];
+function createManeger() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the manager's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?",
+      },
+      {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?",
+      },
+    ])
+    .then((response) => {
+      const maneger = new Maneger(
+        response.name,
+        response.id,
+        response.email,
+        response.officeNumber
+      );
+      teamMembers.push(maneger);
+    })
+    .then(() => chooseEmployee());
+}
+
+//create engineer prompt
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's GitHub username?",
+      },
+    ])
+    .then((response) => {
+      const engineer = new Engineer(
+        response.name,
+        response.id,
+        response.email,
+        response.github
+      );
+      teamMembers.push(engineer);
+    })
+    .then(() => chooseEmployee());
+}
+
+//create intern prompt
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is the intern's id?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "What is the name of the intern's school?",
+      },
+    ])
+    .then((response) => {
+      const intern = new Intern(
+        response.name,
+        response.id,
+        response.email,
+        response.school
+      );
+      teamMembers.push(intern);
+    })
+    .then(() => chooseEmployee());
+}
+
+//call the prompts function
 chooseEmployee();
